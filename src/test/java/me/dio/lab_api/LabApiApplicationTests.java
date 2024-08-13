8e65461cb81b69372a43d3cde2fe91bb7a9335f0
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-08-13 13:49:57 -0300

    initial commit

---
+++ src/test/java/me/dio/lab_api/LabApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.lab_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
